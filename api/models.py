ea7d70f8acbb515b0692afb4619247658f851358
fikayo <soetan@gmail.com>
2022-06-20 22:23:40 +0100

    first commit

---
+++ api/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    middle_name = models.CharField(max_length=25)
+
+    email = models.CharField(max_length=50, unique=True)
+    password = models.BinaryField()
+    username = models.CharField(max_length=20, unique=True)
+
+    is_admin = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    is_super = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    domain = models.TextField()
+
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Log(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # login, logout, message
+    type = models.CharField(max_length=50)
+    # {
+    #   "lat": xxx,
+    #   "long": xxx,
+    # }
+    location = models.TextField()
+    user = User
+    data = models.TextField()
